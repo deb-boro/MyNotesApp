3a729e5e9ef1a31ed53ac4c6270b7a3232c0e1ed
deb boro <debajit.boro666@gmail.com>
2022-07-12 00:37:56 -0400

    updated server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.json())
+app.use(express.static('public'))
 
 function filterByQuery(query, notesArray) {
   let filteredResults = notesArray
@@ -61,6 +61,15 @@ app.post('/api/notes', (req, res) => {
   res.json(note)
 })
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`)
 })
