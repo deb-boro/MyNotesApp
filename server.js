a20b2d7c72dfad4c9a16d39007e5403a532a3986
deb boro <debajit.boro666@gmail.com>
2022-07-11 23:56:16 -0400

    post added

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const { notes } = require('./db/db.json')
+const fs = require('fs')
+const path = require('path')
 const app = express()
 
 const PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
 
 function filterByQuery(query, notesArray) {
   let filteredResults = notesArray
@@ -21,6 +27,16 @@ function findById(id, notesArray) {
   return result
 }
 
+function createNewNotes(body, notesArray) {
+  const note = body
+  notesArray.push(note)
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2),
+  )
+  return note
+}
+
 app.get('/api/notes', (req, res) => {
   let results = notes
   if (req.query) {
@@ -37,6 +53,12 @@ app.get('/api/notes/:id', (req, res) => {
   } else {
     res.send(404)
   }
+})
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString()
+  const note = createNewNotes(req.body, notes)
+  res.json(note)
 })
 
 app.listen(PORT, () => {
