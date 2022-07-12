6bd3e19e1478103db4200c3c60a816f48b1f0e07
deb boro <debajit.boro666@gmail.com>
2022-07-11 21:19:16 -0400

    add heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { notes } = require('./db/db.json')
+const app = express()
+
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray
+
+  if (query.title) {
+    filteredResults = filteredResults.filter(
+      (note) => note.title === query.title,
+    )
+  }
+
+  return filteredResults
+}
+
+app.get('/api/notes', (req, res) => {
+  let results = notes
+  if (req.query) {
+    results = filterByQuery(req.query, results)
+  }
+  res.json(results)
+})
+
+app.listen(3001, () => {
+  console.log('API server now on port 3001')
+})
